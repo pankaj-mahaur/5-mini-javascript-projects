6f5a50ff35e979050c3f4129a48a5a41424fa818
pankaj-mahaur <pankajmahaur2003@gmail.com>
2025-04-05 22:55:35 +0530

    stop watch done

--- script.js
+++ script.js
@@ -157,3 +157,34 @@ function generateQuote() {
 }
 
 //Stop Watch
+
+let secondsElapsed = 0;
+let interval = null;
+const time = document.getElementById("time")
+
+function padStart(value){
+  return String(value).padStart(2, "0")
+}
+function setTime(){
+  const minutes = Math.floor(secondsElapsed / 60)
+  const seconds = secondsElapsed % 60
+  time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`;
+}
+function timer (){
+  secondsElapsed++;
+  setTime();
+}
+function startClock() {
+  if (interval) stopClock()
+  interval = setInterval(timer, 1000)
+}
+
+function stopClock() {
+  clearInterval(interval)
+}
+
+function resetClock() {
+  startClock()
+  secondsElapsed = 0;
+  setTime();
+}
