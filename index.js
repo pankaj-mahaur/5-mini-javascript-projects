dea9025904026571a3545a41320cd803801bfaaa
pankaj-mahaur <pankajmahaur2003@gmail.com>
2025-04-05 19:29:15 +0530

    5 mini js projects

---
+++ index.js
@@ -0,0 +1,15 @@
+const body = document.getElementsByClassName("container1")[0];
+
+function setColor(name) {
+  body.style.backgroundColor = name;
+}
+
+
+function randomColor() {
+    const red = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
+
+    const color = `rgb(${red}, ${green}, ${blue})`
+    body.style.backgroundColor = color;
+}
